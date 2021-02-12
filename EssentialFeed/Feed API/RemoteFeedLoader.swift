e531dab50fc4d5320e167154e29e278a2ee6f564
Vibha <vibhasinghmail@yahoo.com>
2021-02-12 14:30:15 -0500

    Move mapping logic to a new type `FeedItemsMapper`

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -46,9 +46,10 @@ public final class RemoteFeedLoader {
             
             switch result{
             case let .success( response, data):
-                if response.statusCode == 200 , let root =  try? JSONDecoder().decode(Root.self, from: data) {
-                    completion(.success(root.items.map{$0.item}))
-                }else{
+                do{
+                    let items = try FeedItemsMapper.map(data, response)
+                    completion(.success(items))
+                }catch{
                     completion(.failure(.invalidData))
                 }
             case .failure: completion(.failure(.connectivity))
@@ -58,19 +59,34 @@ public final class RemoteFeedLoader {
 }
 
 
-private struct Root: Decodable {
+private class FeedItemsMapper{
     
-    let items : [Item]
-}
+    private struct Root: Decodable {
+        
+        let items : [Item]
+    }
 
-private struct Item: Decodable{
-      
-    let id: UUID
-    let description: String?
-    let location: String?
-    let image: URL
+    private struct Item: Decodable{
+          
+        let id: UUID
+        let description: String?
+        let location: String?
+        let image: URL
+        
+        var item: FeedItem{
+            return FeedItem(id: id, description: description, location: location, imageUrl: image)
+        }
+    }
     
-    var item: FeedItem{
-        return FeedItem(id: id, description: description, location: location, imageUrl: image)
+    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem]{
+        guard response.statusCode == 200 else {
+            throw RemoteFeedLoader.Error.invalidData
+        }
+        let root = try JSONDecoder().decode(Root.self, from: data)
+        return root.items.map{$0.item}
     }
 }
+
+
+
+
