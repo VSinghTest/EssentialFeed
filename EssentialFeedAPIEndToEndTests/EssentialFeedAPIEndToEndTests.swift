92da24f178a1ef90db0fea947740f48428ee94bb
Vibha <vibhasinghmail@yahoo.com>
2021-02-18 22:44:10 -0500

    Add CI scheme

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -36,11 +36,14 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
     //MARK: - Helpers
     
     
-    private func getFeedResult() -> LoadFeedResult?{
+    private func getFeedResult(file: StaticString = #filePath, line: UInt = #line ) -> LoadFeedResult?{
         
         let testServerUrl = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
         let client = URLSessionHTTPClient()
         let loader = RemoteFeedLoader(url: testServerUrl, client: client)
+        
+        trackForMemoryLeaks(instance: client, file: file, line: line)
+        trackForMemoryLeaks(instance: loader, file: file, line: line)
         
         let exp = expectation(description: "Wait for load completion")
         
