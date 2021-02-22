aeda0c11fc4c985dce91e8c7d1b616379901cbfe
Vibha <vibsi.mail@gmail.com>
2021-02-22 18:54:06 -0500

    Validate cache command does not delete cache when cache is already empty

--- EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -113,7 +113,7 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
     }
     
     
-    func test_load_doesNotDeleteCacheOnEmptyCache(){
+    func test_load_hasNoSideEffectsOnEmptyCache(){
         let (sut , store) = makeSUT()
         
         sut.load{ _ in }
