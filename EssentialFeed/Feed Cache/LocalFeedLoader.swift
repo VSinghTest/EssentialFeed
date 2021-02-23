b3e8d44bd768e0100958b47268f8ba34917127a0
Vibha <vibsi.mail@gmail.com>
2021-02-23 12:34:32 -0500

    Extract cache validation policy into the new `FeedCachePolicy` type

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -7,34 +7,56 @@
 // Loading from the cache is a "Query", and ideally should have no side-effects.
 // Deleting the cache alters the state of the system, which is a side-effect.
 
+
+// Localfeedloader implements Use cases (encapsulate application specific business logic) while collaborating with other types so LFL can be called a controller or a control boundary or interactor or model controller
+
+// to fulfill the usecase its need to colloborate eg with a store type that we are hiding behind the feedstore interface, another app detail is how to get the current date also dealing with a synchrony those are all app details that is relevant to the core domain model these r just details
+
+// caching policy is  a policy to a business rule ,dpending on the b rule this policy might be so imptt that needs to be shared among usecases and shared across spplication. so app agnostic. it all depends on the use case. In this case it's a business rule that can be used across usecases / application so can be encapsulate in its own model. we can reuse later if we have to. We can extract the b rules from this control type and moved it to the reusable model
+
+
+//The localfeedloader should encapsulate application-speific logic only, and communicate with Modles to perform business logic.
+// rules and policies (e.g. validation logic) are better suited in a domain model that is application-agnostic (so it can be [re]used across applications.
+
+
 import Foundation
  
-public final class LocalFeedLoader{
+
+private final class FeedCachePolicy{
     
-    private let store: FeedStore
     private let currentDate: () -> Date
     private let calendar = Calendar(identifier: .gregorian)
     
-   
-   
-    
-  public init(store: FeedStore, currentDate:@escaping () -> Date ){
-        self.store = store
+    init(currentDate:@escaping () -> Date ){
         self.currentDate = currentDate
     }
-    
-    
     private var maxCacheAgeInDays: Int{
         return 7
     }
     
-    private func validate(_ timestamp: Date) -> Bool{
+     func validate(_ timestamp: Date) -> Bool{
         guard let maxCacheAge = calendar.date(byAdding: .day,value: maxCacheAgeInDays, to: timestamp) else{ return false }
         return currentDate() < maxCacheAge
     }
     
     
     
+}
+public final class LocalFeedLoader{
+    
+    private let store: FeedStore
+    private let currentDate: () -> Date
+    private let cachePolicy: FeedCachePolicy
+   
+    
+  public init(store: FeedStore, currentDate:@escaping () -> Date ){
+        self.store = store
+        self.currentDate = currentDate
+        self.cachePolicy = FeedCachePolicy(currentDate: currentDate)
+    }
+    
+    
+  
 }
 
 
@@ -72,22 +94,20 @@ extension LocalFeedLoader: FeedLoader{
     public typealias LoadResult = LoadFeedResult
     
     public func load(completion: @escaping (LoadResult) -> Void){
+        
         store.retrieve(){ [weak self] result in
-            
             guard let self = self else { return }
             
             switch result{
             case let .failure(error):
-            
                 completion(.failure(error))
            
-            case let .found(feed, timestamp) where self.validate(timestamp):
+            case let .found(feed, timestamp) where self.cachePolicy.validate(timestamp):
                 completion(.success(feed.toModels()))
             
             case .found, .empty:
                 completion(.success([]))
             }
-            
         }
     }
 }
@@ -101,7 +121,7 @@ extension LocalFeedLoader{
             case .failure:
                 self.store.deleteCachedFeed{_ in }
                 
-            case let .found(_ , timestamp) where !self.validate(timestamp):
+            case let .found(_ , timestamp) where !self.cachePolicy.validate(timestamp):
                 self.store.deleteCachedFeed{ _ in }
                 
             case .empty, .found: break
@@ -114,7 +134,6 @@ extension LocalFeedLoader{
                 // Alternatively, you can add, along with the explicit cases, "@unknown deafult" which will generate a warning (rather than a build error) whne new cases are added.
             }
         }
-       
     }
 }
    
