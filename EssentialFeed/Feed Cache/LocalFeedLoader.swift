8c623fb4024a88ad17a896db5a694dfdb0d69111
Vibha <vibsi.mail@gmail.com>
2021-02-22 22:28:53 -0500

    Make LocalFeedLoader conform to the `FeedLoader`

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -65,7 +65,7 @@ extension LocalFeedLoader{
   
    
     
-extension LocalFeedLoader{
+extension LocalFeedLoader: FeedLoader{
     
     public func load(completion: @escaping (LoadResult) -> Void){
         store.retrieve(){ [weak self] result in
