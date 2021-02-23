ec1e8642a9785b590c596e4676f8033e72770536
Vibha <vibsi.mail@gmail.com>
2021-02-23 17:31:09 -0500

    Retrieving from empty cache twice delivers same empty result (no side-effects)

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -8,18 +8,20 @@
 //    - To empty cache works
 //    - To non-empty cache overrides previous value
 //    - Error (if possible to simulate, e.g., no write permission)
-
+//**************************
 //- Delete
 //    - Empty cache does nothing (cache stays empty and does not fail)
 //    - Inserted data leaves cache empty
 //    - Error (if possible to simulate, e.g., no write permission)
-
+//***************************
 //- Retrieve
 //    - Empty cache works (before something is inserted)
+//    - Empty cache twice returns empty(no side-effects)
 //    - Non-empty cache returns data
 //    - Non-empty cache twice returns same data (retrieve should have no side-effects)
-//    - Error (if possible to simulate, e.g., invalid data)
-
+//    - Error returns error (if possible to simulate, e.g., invalid data)
+//    - Error twice returns same error (if possible to simulate, e.g., invalid data)
+//****************************
 //- Side-effects must run serially to avoid race-conditions (deleting the wrong cache... overriding the latest data...)
 import XCTest
 import EssentialFeed
@@ -46,7 +48,22 @@ class CodableFeedStoreTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
     }
     
-    
-    
-
+    func test_retrieve_hasNoSideEffectsOnEmptyCache(){
+        let sut = CodableFeedStore()
+       let exp = expectation(description: "Wait for cache retrieval")
+        sut.retrieve{ firstResult in
+            sut.retrieve{secondResult in
+            switch (firstResult,secondResult) {
+            case (.empty, .empty):
+                break
+            default:
+                XCTFail("Expected retrieving twice from empty cache to deliver same empty result, got \(firstResult) and \(secondResult) instead")
+            }
+            exp.fulfill()
+            }
+            
+        }
+        wait(for: [exp], timeout: 1.0)
+       
+    }
 }
