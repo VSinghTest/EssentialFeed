e06ca7821868d6b1b9a691c14bb571af3d424c95
Vibha <vibsi.mail@gmail.com>
2021-02-24 15:37:53 -0500

    Retrieval delivers failure on retrieval error(invalid cached data)

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -65,11 +65,15 @@ class CodableFeedStore{
         guard let data = try? Data(contentsOf: storeURL) else{
             return completion(.empty)
         }
-        let decoder = JSONDecoder()
-        let cache = try! decoder.decode(Cache.self, from: data)
-        completion(.found(feed: cache.localFeed , timestamp: cache.timestamp))
+        do{
+            let decoder = JSONDecoder()
+            let cache = try decoder.decode(Cache.self, from: data)
+            completion(.found(feed: cache.localFeed , timestamp: cache.timestamp))
+       
+        }catch{
+            completion(.failure(error))
+         }
     }
-    
     
     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertionCompletion){
         
@@ -136,6 +140,13 @@ class CodableFeedStoreTests: XCTestCase {
         expect(sut, toRetrieveTwice: .found(feed: feed, timestamp: timestamp))
     }
 
+    func test_retrieve_deliversFailureOnRetrivalError(){
+        let sut = makeSUT()
+        
+        try! "invalid data".write(to: testSpecificStoreURL(), atomically: false, encoding: .utf8)
+        
+        expect(sut, toRetrieve: .failure(anyNSError()))
+    }
     
     //MARK: - Helpers
     
@@ -163,7 +174,7 @@ class CodableFeedStoreTests: XCTestCase {
          
         sut.retrieve{ retrieveResult in
              switch (expectedResult,retrieveResult) {
-             case (.empty, .empty):
+             case (.empty, .empty),(.failure, .failure):
                  break
             
              case let(.found(expected), .found(retrieved)):
