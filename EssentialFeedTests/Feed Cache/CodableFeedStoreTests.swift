04f4f74183641c553d5e972bb39631171b6f70e3
Vibha <vibsi.mail@gmail.com>
2021-02-23 21:42:06 -0500

    Retrieving after inserting to empty cache delivers inserted values

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -27,12 +27,53 @@ import XCTest
 import EssentialFeed
 
 class CodableFeedStore{
+    
+    private struct Cache: Codable{
+        let feed: [LocalFeedImage]
+        let timestamp: Date
+    }
+    
+    private let storeURL = FileManager.default.urls(for: .documentDirectory, in : .userDomainMask).first!.appendingPathComponent("image-feed.store")
+    
+    
     func retrieve(completion:@escaping FeedStore.RetrievalCompletion){
-        completion(.empty)
+        guard let data = try? Data(contentsOf: storeURL) else{
+            return completion(.empty)
+        }
+        let decoder = JSONDecoder()
+        let cache = try! decoder.decode(Cache.self, from: data)
+        completion(.found(feed: cache.feed , timestamp: cache.timestamp))
+    }
+    
+    
+    func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertionCompletion){
+        
+        let encoder = JSONEncoder()
+        let encoded = try! encoder.encode(Cache(feed: feed, timestamp: timestamp))
+        try! encoded.write(to: storeURL)
+        completion(nil)
     }
 }
-class CodableFeedStoreTests: XCTestCase {
 
+class CodableFeedStoreTests: XCTestCase {
+    
+ //   this is the downside of not mocking the file system, good side is we are testing the real implementation but it leaves artifacts,side-effects,they may affect other tests or whole system. So we need to remove artifcats
+    
+    override func setUp() {
+        super.setUp()// now test shoul know abt the internals of the test =>url
+        
+        let storeURL = FileManager.default.urls(for: .documentDirectory, in : .userDomainMask).first!.appendingPathComponent("image-feed.store")
+        try? FileManager.default.removeItem(at: storeURL)
+         }
+    
+    override func tearDown() {// is invoked after every test method execution. but there is another problem very hard to debug. sometimes teardown method is not invoked, eg if you have crash in your system or even if you set a break point and if you finished the execution of a test before it finish running. so to pass this prob you have to write a  set up method
+        super.tearDown()
+        
+        let storeURL = FileManager.default.urls(for: .documentDirectory, in : .userDomainMask).first!.appendingPathComponent("image-feed.store")
+        try? FileManager.default.removeItem(at: storeURL)
+         }
+
+ //Mark: - Retrival
     func test_retrieve_deliversemptyOnEmptyCache(){
         let sut = CodableFeedStore()
        let exp = expectation(description: "Wait for cache retrieval")
@@ -64,6 +105,33 @@ class CodableFeedStoreTests: XCTestCase {
             
         }
         wait(for: [exp], timeout: 1.0)
-       
-    }
+  }
+    
+    //Mark: - Insertion
+    
+    func test_retrieveAfterInsertingToEmptyCache_deliversInsertedValues(){
+        let sut = CodableFeedStore()
+        let feed = uniqueImageFeed().local
+        let timestamp = Date()
+        
+       let exp = expectation(description: "Wait for cache retrieval")
+        sut.insert(feed, timestamp: timestamp){ insertionError in
+            XCTAssertNil(insertionError, "Expected feed to be inserted successfully")
+            
+            sut.retrieve{retrieveResult in
+                switch (retrieveResult) {
+                case let .found(retrivedFeed, retrievedTimestamp):
+                    XCTAssertEqual(retrivedFeed, feed)
+                    XCTAssertEqual(retrievedTimestamp, timestamp)
+                default:
+                    XCTFail("Expected found result with \(feed) and timestamp \(timestamp), got \(retrieveResult) instead")
+                }
+                exp.fulfill()
+            }
+            
+        }
+        wait(for: [exp], timeout: 1.0)
+  }
 }
+
+
