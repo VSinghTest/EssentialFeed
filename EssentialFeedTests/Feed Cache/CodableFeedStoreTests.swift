a8675047c09a159953c47217eae5f9cb711bad0b
Vibha <vibsi.mail@gmail.com>
2021-02-25 17:50:45 -0500

    Break down `CodeFeedStore` tests to guarantee there's only one assertion per test. The goal is to clarify the behavior under test in small units, so we can extract the behavior tests into reusable specs

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -26,6 +26,39 @@
 import XCTest
 import EssentialFeed
 
+protocol FeedStoreSpecs{
+  
+     func test_retrieve_deliversEmptyOnEmptyCache()
+     func test_retrieve_hasNoSideEffectsOnEmptyCache()
+     func test_retrieve_deliversFoundValuesOnNonEmptyCache()
+     func test_retrieve_hasNoSideEffectsOnNonEmptyCache()
+    
+    
+     func test_insert_overridesPreviouslyInsertedCacheValues()
+    
+    
+     func test_delete_hasNoSideEffectsOnEmptyCache()
+     func test_delete_emptiesPreviouslyInsertedCache()
+     
+    
+     func test_storeSideEffects_runSerially()
+    
+}
+// this exposes more test cases for our clients to implement. every test now one assertion per test which is a good guideline to follow. try to have only one assertion per test
+protocol FailableRetrieveFeedStoreSpecs{
+    func test_retrieve_deliversFailureOnRetrivalError()
+    func test_retrieve_hasNoSideEffectsOnFailure()
+}
+
+protocol  FailableInsertFeedStoreSpecs{
+    func test_insert_deliversErrorOnInsertionError()
+    func test_insert_hasNoSideEffectsOnInsertionError()
+}
+
+protocol FailableDeleteFeedStoreSpecs{
+    func test_delete_deliversErrorOnDeletionError()
+    func test_delete_hasNoSideEffectsOnDeletionError()
+}
 
 class CodableFeedStoreTests: XCTestCase {
     
@@ -127,6 +160,17 @@ class CodableFeedStoreTests: XCTestCase {
         XCTAssertNotNil(insertionError, "expected cache insertion to fail with an error")
     }
     
+    func test_insert_hasNoSideEffectsOnInsertionError(){
+        let invalidStoreURL = URL(string: "invalid://store-url")!
+        let sut = makeSUT(storeURL: invalidStoreURL)
+        let feed = uniqueImageFeed().local
+        let timestamp = Date()
+        
+        insert((feed, timestamp), to : sut)
+        
+        expect(sut, toRetrieve: .empty)
+    }
+    
     //MARK: -  Deletion
     
     
@@ -164,7 +208,16 @@ class CodableFeedStoreTests: XCTestCase {
             expect(sut, toRetrieve: .empty)
         }
     
-    
+    func test_delete_hasNoSideEffectsOnDeletionError(){
+        let noDeletePermissionURL = cachesDirectory()
+        let sut = makeSUT(storeURL: noDeletePermissionURL)//
+
+        let deletionError = deleteCache(from: sut)
+
+        XCTAssertNotNil(deletionError, "Expected cache deletion to fail")
+        expect(sut, toRetrieve: .empty)
+        
+    }
     func test_storeSideEffects_runSerially(){
         
         let sut = makeSUT()
