1f5d7df971be164ad18385ad24250870cf10599b
Vibha <vibsi.mail@gmail.com>
2021-02-26 15:55:25 -0500

    Extract reusable CoreData helpers into a separate file

--- EssentialFeed/Feed Cache/CoreDataFeedStore.swift
+++ EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -22,8 +22,7 @@ public final class CoreDataFeedStore: FeedStore {
 
     public func retrieve(completion: @escaping RetrievalCompletion) {
        
-        let context = self.context
-                context.perform {
+        perform { context in
                     do {
                         if let cache = try ManagedCache.find(in: context) {       completion(.found(feed: cache.localFeed, timestamp: cache.timestamp))
                         } else {
@@ -36,8 +35,7 @@ public final class CoreDataFeedStore: FeedStore {
     }
 
     public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-        let context = self.context
-                context.perform {
+        perform { context in
                     do {
                         let managedCache = try ManagedCache.newUniqueInstance(in: context)
                         managedCache.timestamp = timestamp
@@ -52,8 +50,7 @@ public final class CoreDataFeedStore: FeedStore {
     }
 
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-        let context = self.context
-                context.perform {
+        perform { context in
                     do {
                         try ManagedCache.find(in: context).map(context.delete).map(context.save)
                         completion(nil)
@@ -62,39 +59,15 @@ public final class CoreDataFeedStore: FeedStore {
                     }
                 }
     }
-
-}
-
-private extension NSPersistentContainer {
-    enum LoadingError: Swift.Error {
-        case modelNotFound
-        case failedToLoadPersistentStores(Swift.Error)
-    }
-
-    static func load(modelName name: String, url: URL, in bundle: Bundle) throws -> NSPersistentContainer {
-            guard let model = NSManagedObjectModel.with(name: name, in: bundle) else {
-            throw LoadingError.modelNotFound
+    
+    private func perform(_ action: @escaping (NSManagedObjectContext) -> Void) {
+            let context = self.context
+            context.perform { action(context) }
         }
 
-        let description = NSPersistentStoreDescription(url: url)
-                let container = NSPersistentContainer(name: name, managedObjectModel: model)
-                container.persistentStoreDescriptions = [description]
-
-        var loadError: Swift.Error?
-        container.loadPersistentStores { loadError = $1 }
-        try loadError.map { throw LoadingError.failedToLoadPersistentStores($0) }
-
-        return container
-    }
 }
 
-private extension NSManagedObjectModel {
-    static func with(name: String, in bundle: Bundle) -> NSManagedObjectModel? {
-        return bundle
-            .url(forResource: name, withExtension: "momd")
-            .flatMap { NSManagedObjectModel(contentsOf: $0) }
-    }
-}
+
 
 
 @objc(ManagedCache)
