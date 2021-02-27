618fcf5a0c4b83451bd6bc40a17f3fbd4f77d468
Vibha <vibsi.mail@gmail.com>
2021-02-27 12:37:50 -0500

    Add typealias for `FeedStore.DeletionResult` and `FeedStore.InsertionResult`

--- EssentialFeed/Feed Cache/FeedStore.swift
+++ EssentialFeed/Feed Cache/FeedStore.swift
@@ -12,9 +12,12 @@ public typealias  CachedFeed = (feed: [LocalFeedImage], timestamp: Date)
 // In FeedStore case the side effects overlapped. when u delete a cache u affect the insert. when u insert u also affect the delete and reteive affected by all of them. this can become messy quickly. so before we begin we must fully understand our expectations on the infrastructure implementation.
 
 public protocol FeedStore{
+    typealias DeletionResult = Error?
+    typealias DeletionCompletion = (DeletionResult) -> Void
     
-    typealias DeletionCompletion = (Error?) -> Void
-    typealias InsertionCompletion = (Error?) -> Void
+    typealias InsertionResult = Error?
+    typealias InsertionCompletion = (InsertionResult) -> Void
+    
     typealias RetrievalResult = Result<CachedFeed?, Error>
     typealias RetrievalCompletion = (RetrievalResult) -> Void
     
