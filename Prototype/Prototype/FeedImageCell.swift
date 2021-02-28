4bb102909bdd4416c7e73134d01d0ac29dbfc8b1
Vibha <vibsi.mail@gmail.com>
2021-02-28 08:14:11 -0500

    Add `FeedImageCell` with storyboard connections

---
+++ Prototype/Prototype/FeedImageCell.swift
@@ -0,0 +1,20 @@
+//
+//  FeedImageCell.swift
+//  Prototype
+//
+//  Created by Vibha Singh on 2/28/21.
+//
+
+import UIKit
+
+class FeedImageCell: UITableViewCell{
+    
+    @IBOutlet weak var locationContainer: UIStackView!
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    @IBOutlet weak var feedImageView: UIImageView!
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+}
